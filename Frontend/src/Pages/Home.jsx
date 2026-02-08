9c00b6ec2e6ff3aa17ba80c1dc8937e23591271e
sachin <sachinjurel82@gmail.com>
2026-02-08 13:08:42 +0530

    Design Hero Section

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <div className="w-full md:mt-2 mt-20 md:w-1/2 h-[300px] sm:h-[400px] md:h-[600px] flex justify-center">
           <Model />
         </div>
-        <div className="-mt-94 sm:-mt-68 md:-mt-120 w-full flex absolute left h-15 bg-black"></div>
+        <div className="-mt-94 sm:-mt-68 md:-mt-130 w-full flex absolute left h-15 bg-black"></div>
         <div className="w-full md:w-1/2 flex justify-center">
           <Hero />
         </div>
